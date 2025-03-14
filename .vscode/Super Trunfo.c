0b45d7a63ca6d196821cbc4372f7ee0861e14ba1
celio02 <celiovolica@hotmail.com>
2025-03-14 18:46:30 +0000

    desafio novato super Trunfo.

---
+++ .vscode/Super Trunfo.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+int main(){
+
+char carta1[30]; //codigo_carta
+char estado1[50]; //nome_estado
+char cidade1[50]; //nome_cidade
+int pontos; //quantidade_pontosTuristicos
+int populacao; //quantidade_habitantes
+float area; //area_Total
+float PIB; //valor_PIB
+
+printf("Bem vindos ao jogo super trunfo,preencha o valor de cada card: \n");
+
+printf("digite o codigo da carta: \n");
+scanf("%s", carta1);
+
+printf("digite o nome do estado: \n");
+scanf("%s", estado1);
+
+printf("digite o nome da cidade: \n");
+scanf("%s", cidade1);
+
+printf("digite a quantidade pontos turisticos: \n");
+scanf("%d", &pontos);
+
+printf("digite a quantidade de habitantes: \n");
+scanf("%d", &populacao);
+
+printf("digite a area em km quadrados: \n");
+scanf("%e", &area);
+
+printf("digite o PIB: \n");
+scanf("%f", &PIB);
+
+char carta2[30];//codigo_carta2
+char estado2[30];//codigo_carta2
+char cidade2[30];//nome_cidade2
+int pontos2;//quantidade_pontosTuristicos
+int populacao2;//quantidade_habitantes
+float area2;//areaTotal
+float PIB2;//valor_PIB
+
+printf("agora preecha o segundo card: \n");
+printf("digite o codigo ds carta2: \n");
+scanf("%s, carta2");
+
+printf("digite o nome do estado2: \n");
+scanf("%s, estado");
+
+printf("digite o nome da cidade2: \n");
+scanf("%s, cidade2");
+
+printf("digite a quantidade de pontos turisticos: \n");
+scanf("%d, &pontos2");
+
+printf("digite a quantidade de habitantes: \n");
+scanf("%d, &populacao2");
+
+printf("digite a area em km quadrados: \n");
+scanf("%e, &area2");
+
+printf("digite o PIB: \n ");
+scanf("%f, &PIB2");
+
+return 0;
+
+
+
+
+
+
+
+
+}
---
+++ desafio cartas super tru/Cadastrar Cartas.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+int main(){
+
+    char Estado[50]; //Nome Estado
+    char Cidade [30]; //Nome da Cidade 
+    char carta [30]; //Código da carta 
+    int População; //Número de Habitantes 
+    int pontos; //Número de pontos Turisticos 
+    float área; //Área da cidade em km quadrados
+    float PIB; //Produto interno Bruto
+
+    printf("Digite o nome do estado: \n");
+    scanf("%c", &Estado);
+
+    printf("digite o nome da Cidade: \n");
+    scanf("%c", &Cidade);
+
+    printf("digite o codigo da carta: \n");
+    scanf("%c", &carta);
+
+    printf("Digite o número de habitantes: \n");
+    scanf("%d", &População);
+
+    printf("Digite o número de pontos turisticos: \n");
+    scanf("%d", &pontos);
+
+    printf("Digite a área em km quadrados: \n");
+    scanf("%f", &área);
+
+    printf("Digite o produto interno bruto: \n");
+    scanf("%f", &PIB);
+
+    return 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
